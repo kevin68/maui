0b4cd2aadc547a1959184d0dbffd8d09c54f4e62
Tamilarasan Paranthaman <93904422+Tamilarasan-Paranthaman@users.noreply.github.com>
2026-06-29 16:17:15 +0530

    Change handler as default

--- src/Core/src/RuntimeFeature.cs
+++ src/Core/src/RuntimeFeature.cs
@@ -29,7 +29,6 @@ namespace Microsoft.Maui
 		const bool EnableAspireByDefault = true;
 		const bool IsMaterial3EnabledByDefault = false;
 		const bool IsCssEnabledByDefault = true;
-		const bool IsUseiOSTabbedViewHandlerEnabledByDefault = false;
 
 #pragma warning disable IL4000 // Return value does not match FeatureGuardAttribute 'System.Diagnostics.CodeAnalysis.RequiresUnreferencedCodeAttribute'. 
 #if NET9_0_OR_GREATER
@@ -168,12 +167,5 @@ namespace Microsoft.Maui
 				? isEnabled
 				: IsCssEnabledByDefault;
 
-#if NET11_0_OR_GREATER
-		[FeatureSwitchDefinition($"{FeatureSwitchPrefix}.{nameof(UseiOSTabbedViewHandler)}")]
-#endif
-		public static bool UseiOSTabbedViewHandler =>
-			AppContext.TryGetSwitch($"{FeatureSwitchPrefix}.{nameof(UseiOSTabbedViewHandler)}", out bool isEnabled)
-				? isEnabled
-				: IsUseiOSTabbedViewHandlerEnabledByDefault;
 	}
 }
